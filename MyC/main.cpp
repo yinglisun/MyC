262989afd29d542c4515c7c47b7fce5e537b5c9f
孙英力 <yinglisun@MyiMac-2.local>
2012-11-05 22:48:50 +0800

    second commit

--- MyC/main.cpp
+++ MyC/main.cpp
@@ -1,11 +1,13 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!" << endl;
     return 0;
 }
 
