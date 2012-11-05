db4339c63ae553b378e020c50b2817fa15d87a28
孙英力 <yinglisun@MyiMac-2.local>
2012-11-05 22:31:18 +0800

    Initial Commit

---
+++ MyC/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MyC
+//
+//  Created by 孙英力 on 11/5/12.
+//  Copyright (c) 2012 孙英力. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
