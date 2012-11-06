f3fe98174429575d78cda5fc38f6661409e7782f
Yingli Sun <yingli64.sun@gmail.com>
2012-11-06 08:57:48 +0800

    changes in office to use list

--- MyC/main.cpp
+++ MyC/main.cpp
@@ -1,13 +1,20 @@
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
+    vector<int> vecInteger;
+    for (int i=0; i<10; i++) {
+        vecInteger.push_back(i);
+    }
+    
+    int nNum = 0;
+    for_each(vecInteger.cbegin(), vecInteger.cend(), [&nNum](int n) { ++nNum; cout << n << ' '; });
+    cout << ": Total " << nNum << " integers" << endl;
 
-    // insert code here...
-    cout << "Hello, World!" << endl;
     return 0;
 }
 
